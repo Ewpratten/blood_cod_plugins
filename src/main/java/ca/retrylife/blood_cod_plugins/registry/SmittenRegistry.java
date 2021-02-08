0ea30790a7be9d3622687486ddc04f08bd3af57b
Evan Pratten <ewpratten@gmail.com>
2021-02-08 10:41:55 -0500

    redo smitten registry

--- src/main/java/ca/retrylife/blood_cod_plugins/registry/SmittenRegistry.java
+++ src/main/java/ca/retrylife/blood_cod_plugins/registry/SmittenRegistry.java
@@ -1,7 +1,9 @@
 package ca.retrylife.blood_cod_plugins.registry;
 
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.entity.LivingEntity;
 
 public class SmittenRegistry {
 
@@ -20,16 +22,44 @@ public class SmittenRegistry {
         return instance;
     }
 
-    // Victim
+    // Victim registry
+    private HashMap<UUID, Double> registry = new HashMap<>();
 
     // Hidden constructor to force singleton usage
     private SmittenRegistry() {
     }
 
-    public void setMostRecentVictim(Entity entity) {
+    public void setMostRecentVictim(LivingEntity entity) {
+
+        // Get unique id of the victim
+        UUID uuid = entity.getUniqueId();
+
+        // Get current timestamp
+        double timestamp = System.currentTimeMillis();
+
+        // Set the registry entry
+        this.registry.put(uuid, timestamp);
+
     }
 
-    public boolean isFinalBlow(Entity entity) {
+    public boolean isFinalBlow(LivingEntity entity) {
+
+        // Get unique id of the victim
+        UUID uuid = entity.getUniqueId();
+
+        // Get current timestamp
+        double timestamp = System.currentTimeMillis();
+
+        // Check for match
+        if (this.registry.containsKey(uuid)) {
+
+            // Check that the time range is reasonable
+            if ((timestamp - this.registry.get(uuid)) <= 1500) {
+                return true;
+            }
+
+        }
+
         return false;
     }
 
